e1477453295cb536a3c8db0a426b33297ddb2e1c
Alex <Lepiozdyx@gmail.com>
2024-12-13 09:42:14 -0300

    Initial Commit

---
+++ Horseshoeonline/HorseshoeonlineApp.swift
@@ -0,0 +1,17 @@
+//
+//  HorseshoeonlineApp.swift
+//  Horseshoeonline
+//
+//  Created by Alex on 13.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct HorseshoeonlineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
